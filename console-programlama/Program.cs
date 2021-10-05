889487c63606849675cf6c8783722db3cb775522
berkay <berkayoruc@hotmail.com>
2021-10-05 10:37:43 +0300

    first commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            string surname = Console.ReadLine();
+            Console.WriteLine(name + " " + surname);
+        }
+    }
+}
