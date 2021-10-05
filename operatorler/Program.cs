b326420eaf815114bc346ea33b5e4edcfb228b1e
berkay <berkayoruc@hotmail.com>
2021-10-05 16:14:11 +0300

    degisken

---
+++ operatorler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3; 
+            int y = 3;
+            y = y+2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 3;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+            
+            if (isSuccess && isCompleted) {
+                Console.WriteLine("selam");
+            }
+            if (isSuccess || isCompleted) {
+                Console.WriteLine("merhaba");
+            }
+            if (isSuccess || !isCompleted) {
+                Console.WriteLine("hmm");
+            }
+        }
+    }
+}
