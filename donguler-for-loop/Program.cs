7059f7e7bc64e987b16fb9e82d6d5b57fca3b9ec
berkay <berkayoruc@hotmail.com>
2021-10-06 15:08:58 +0300

    donguler-for-loop

---
+++ donguler-for-loop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace donguler_for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı gir");
+            int length = int.Parse(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
