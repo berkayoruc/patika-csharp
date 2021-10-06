ae14688e45fad4dd7f02a2dc23aaeef8e6eb730d
berkay <berkayoruc@hotmail.com>
2021-10-06 15:24:18 +0300

    donguler-while-foreach

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+            string[] arabalar = { "bmw", "ford", "toyota", "nissan" };
+            foreach (var item in arabalar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
