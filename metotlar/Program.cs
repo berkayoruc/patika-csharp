dec13a4b26b0bf8912aaa62b7c6e0d0a7aee3a46
berkay <berkayoruc@hotmail.com>
2021-10-07 11:52:16 +0300

    metotlar

---
+++ metotlar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // erisim_belirteci geri_donustipi metot_adi(parametrelistesi)
+            // {
+            //      komutlar
+            // }
+
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a + b);
+            Console.WriteLine(Topla(a, b));
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(Topla(a, b)));
+            ornek.EkranaYazdir(ornek.ArttirVeTopla(ref a, ref b).ToString());
+        }
+
+        static int Topla(int x, int y)
+        {
+            return (x + y);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        // Orijinal değerler üzerinden işlem yapılır
+        // İlgili parametreler globalde değişir
+        public int ArttirVeTopla(ref int x, ref int y)
+        {
+            x += 1;
+            y += 1;
+            return x + y;
+        }
+    }
+}
