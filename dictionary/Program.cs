31a20ffa95bb15046491519df9b0a87195f50c3b
berkay <berkayoruc@hotmail.com>
2021-10-11 14:45:33 +0300

    dictionary

---
+++ dictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dictionary<int, string> kullanicilar = new Dictionary<int, string>();
+
+           kullanicilar.Add(0, "aaa");
+           kullanicilar[34] = "llll";
+
+           Console.WriteLine(kullanicilar[34]);
+
+           foreach (var item in kullanicilar)
+           {
+               Console.WriteLine(item);
+           }
+
+           Console.WriteLine(kullanicilar.Count);
+           Console.WriteLine(kullanicilar.ContainsKey(34));
+           Console.WriteLine(kullanicilar.ContainsValue("aa"));
+
+           kullanicilar.Remove(0);
+
+           Console.WriteLine(kullanicilar.Keys);
+           Console.WriteLine(kullanicilar.Values);
+        }
+    }
+}
