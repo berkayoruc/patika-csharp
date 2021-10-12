6c51aaba0c79f0aa7c4f980d941c6f63fecab59b
berkay <berkayoruc@hotmail.com>
2021-10-12 15:19:20 +0300

    csharp-enum

---
+++ csharp-enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Cuma);
+            Console.WriteLine((int)Gunler.Cuma);
+
+            int temp = 32;
+
+            if (temp <= (int)Temperature.Normal)
+            {
+                Console.WriteLine("aaaaaaaa");
+            } else {
+                Console.WriteLine("vvvvvv");
+            }
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba,
+        Perşembe = 53,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum Temperature
+    {
+        Cold = 5,
+        Normal = 20,
+        Hot = 25
+
+    }
+}
